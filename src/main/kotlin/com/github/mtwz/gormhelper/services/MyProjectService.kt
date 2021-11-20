8958e151aab058eddac601f8cc5de7563d13f914
GitHub Action <action@github.com>
2021-11-20 05:15:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/mtwz/gormhelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mtwz/gormhelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mtwz.gormhelper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mtwz.gormhelper.MyBundle
 
 class MyProjectService(project: Project) {
 
